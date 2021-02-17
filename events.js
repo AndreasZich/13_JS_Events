fc9e84f0d4c9559ef76140a23efb506ee69a41df
Thuldur <john.t.williams1984@gmail.com>
2021-02-17 08:41:10 +0100

    Lösung

--- events.js
+++ events.js
@@ -3,3 +3,51 @@
 function ausgabe(outputStr) {
     console.log(outputStr);
 }
+
+// Definition des Buttons
+
+let modus;
+let btn = document.getElementById("trigBtn");
+
+
+btn.addEventListener("click",dayMode);
+btn.addEventListener("click",toggleText);
+btn.addEventListener("click",nightMode);
+
+
+
+//actOnClick();
+/*function actOnClick() {
+    ausgabe("Click abgefangen");
+}*/
+
+function nightMode() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+    toggleText;
+  }
+
+  function dayMode() {
+    let element = document.getElementById("trigBtn");
+    element.classList.toggle("white-mode");
+    toggleText;
+
+  }
+
+
+  function toggleText(trigBtn) 
+{
+   let el = document.getElementById("trigBtn").innerHTML;
+   if (el == "Dark-Mode") 
+   {
+    ausgabe("Day Mode");
+       document.getElementById("trigBtn").innerHTML = "Day-Mode";
+       
+   }
+   else 
+   {
+    ausgabe("Night Mode");
+    document.getElementById("trigBtn").innerHTML = "Dark-Mode";
+     
+   }
+}
